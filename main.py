3ec39d7f8201b38f6e6fa3766b26cde9b04d6786
Anupriya Johari <anupriyajo@gmail.com>
2021-03-11 18:41:43 +0000

    [CA-2] add output folder
    Co-authored-by: Rahul De <rahul080327@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import asyncio
 import json
+import os
 from typing import List
 
 import aiofiles
 import aiohttp
 
 
-async def download_image(image_url: str, session: aiohttp.ClientSession) -> str:
-    file_name = image_url.split("/")[-1]
+async def download_image(image_url: str, session: aiohttp.ClientSession, path: str) -> str:
+    file_name = os.path.join(path, image_url.split("/")[-1])
     async with session.get(image_url) as response:
         img_file = await aiofiles.open(file_name, mode="wb")
         await img_file.write(await response.read())
@@ -20,14 +21,18 @@ async def read_file(file_name: str) -> List[str]:
         return json.loads(await urls.read()).get("urls")
 
 
-async def main():
+async def main(path: str):
     urls = await read_file("images.json")
     tasks = []
     async with aiohttp.ClientSession() as session:
         for url in urls:
-            tasks.append(asyncio.ensure_future(download_image(url, session)))
+            tasks.append(asyncio.ensure_future(download_image(url, session, path)))
         await asyncio.gather(*tasks)
 
+
 if __name__ == '__main__':
+    output = "output"
+    if not os.path.exists(output):
+        os.mkdir(output)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main(output))
