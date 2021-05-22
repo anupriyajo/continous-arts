338286e403171f845cab43b506de71bcfe81af6a
Rahul De <rahul080327@gmail.com>
2021-05-22 11:41:16 +0200

    more pythonic code

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import boto3
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 import json
 import urllib
 import os
+
+import boto3
 from spellchecker import SpellChecker
 
 spell = SpellChecker()
@@ -11,58 +16,68 @@ SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
 REGION_NAME = "us-west-2"
 
 # Amazon Textract client
-textract = boto3.client('textract', aws_access_key_id=ACCESS_ID, aws_secret_access_key=SECRET_KEY,
-                        region_name=REGION_NAME)
+textract = boto3.client(
+    "textract",
+    aws_access_key_id=ACCESS_ID,
+    aws_secret_access_key=SECRET_KEY,
+    region_name=REGION_NAME,
+)
 
 
-def read_file(f):
-    with open(f) as json_file:
+def read_file(path):
+    with open(path) as json_file:
         data = json.load(json_file)
-    return (data['urls'])
+
+    return data["urls"]
 
 
-def download_file(url):
-    file_name = url.split("/")[-1]
-    urllib.request.urlretrieve(url, file_name)
+def download_file(file_url):
+    file_name = file_url.split("/")[-1]
+    urllib.request.urlretrieve(file_url, file_name)
+
     return file_name
 
 
 def check_spelling(word):
     misspelled = spell.unknown([word])
-    if len(misspelled) > 0:
-        return spell.correction(word)
-    else:
-        return (word)
+
+    return spell.correction(word) if misspelled else word
 
 
-def run_textract(im):
-    documentText = ""
+def run_textract(image):
+    document_text = ""
     found_words = []
-    with open(im, 'rb') as document:
-        imageBytes = bytearray(document.read())
-    response = textract.detect_document_text(Document={'Bytes': imageBytes})
+
+    with open(image, "rb") as document:
+        image_bytes = bytearray(document.read())
+
+    response = textract.detect_document_text(Document={"Bytes": image_bytes})
+
     for item in response["Blocks"]:
         if item["BlockType"] == "LINE":
-            documentText = documentText + item["Text"]
-            phrase = item["Text"].split()
-            for p in phrase:
-                found_words.append(p)
+            document_text = document_text + item["Text"]
+            found_words += item["Text"].split()
+
     return found_words
 
 
-def spellCheck(words):
+def spell_check(words):
     corrected_text = []
-    for fw in words:
-        corr = check_spelling(fw)
-        if check_spelling(fw) != 'none':
+
+    for word in words:
+        corr = check_spelling(word)
+
+        if check_spelling(word) != "none":
             corrected_text.append(corr)
-    return (corrected_text)
+
+    return corrected_text
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     urls = read_file("images.json")
+
     for url in urls:
         im = download_file(url)
-        words = run_textract(im)
-        corrected = spellCheck(words)
+        all_words = run_textract(im)
+        corrected = spell_check(all_words)
         print(corrected)
